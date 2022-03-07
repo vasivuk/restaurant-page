d44ff694ade38e63daa2f90b678515a578006e52
vasivuk <vasivuk@yahoo.com>
2022-03-07 17:46:15 +0100

    Added header

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import loadHome from "./home";
 import loadMenu from "./menu";
 import loadContact from "./contact";
 
+const content = document.querySelector('.content');
+
+function initializeNavbar(){
+    const header = document.createElement('div');
+    header.classList.add('header');
+    const home = document.createElement('div');
+    home.innerHTML = '<button>Home</button>';
+
+    const menu = document.createElement('div');
+    menu.innerHTML = '<button>Menu</button>';
+
+    const contact = document.createElement('div');
+    contact.innerHTML = '<button>Contact</button>';
+
+    header.appendChild(home);
+    header.appendChild(menu);
+    header.appendChild(contact);
+    content.appendChild(header);
+}
+initializeNavbar();
 loadHome();
 loadMenu();
 loadContact();
